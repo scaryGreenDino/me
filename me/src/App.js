ef021cdf6be0e5ef5f1ae0c409e39edcc8a87cad
jordan.c <jordan.c@awsys.com>
2021-06-23 17:08:49 -0500

    Little rename

---
+++ me/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import HomePage from './Components/HomePage.jsx.js' 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          hey.
+        </p>
+        <HomePage></HomePage>
+      </header>
+    </div>
+  );
+}
+
+export default App;
