6be4f0e53c4e0c7acb1aa1af54e1c14f57b57b42
jordan.c <jordan.c@awsys.com>
2021-06-24 09:29:50 -0500

    Little typey type

--- me/src/Components/HomePage.jsx
+++ me/src/Components/HomePage.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import "../App.css";
 var HomePage = () => {
+  const [words, setWords] = useState("")
+  const [count, setCount] = useState(0);
+  const text = "hey, i'm jordan";
+  
+  useEffect(() => {
+    if (count < text.length) {
+        const r = 100 + Math.random() * 200;
+      setTimeout(() => {
+        setCount(count + 1);
+      }, r);
+      }
+
+  },[count,text])
   return (
     <div className="center-wrapper">
-      <div className="center-content">hey, i'm jordan</div>
+      <div className="center-content">{text.substring(0,count)}</div>
     </div>
   );
 };
