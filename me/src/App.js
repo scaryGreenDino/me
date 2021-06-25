2bc24cb3597c3cef052871cfb4b0163d21e3c159
jordan.c <jordan.c@awsys.com>
2021-06-24 22:41:19 -0500

    Some blinking

--- me/src/App.js
+++ me/src/App.js
@@ -4,13 +4,7 @@ import HomePage from './Components/HomePage.jsx'
 function App() {
   return (
     <div className="App">
-      <div className="wrapper">
-        <div className="top-left-wrapper" />
-        <div className="bottom-right-wrapper" />
-        <div className="right-wrapper" />
-
         <HomePage />
-      </div>
     </div>
   );
 }
--- me/src/Components/HomePage.jsx
+++ me/src/Components/HomePage.jsx
@@ -17,10 +17,34 @@ useEffect(() => {
   return (<p>{ text.substring(0, count) }</p>)
 }
 var HomePage = () => {
+  const [words, setWords] = useState("");
+  const [count, setCount] = useState(42);
+  const text = "hey, i'm jordan";
+
+  useEffect(() => {
+      const r = Math.random() * 1000;
+      setTimeout(() => {
+        setCount(count + 1);
+      }, r);
+  }, [count, text]);
   
   return (
-    <div className="center-wrapper">
-      <div className="center-content"><TextType /> </div>
+    <div className="wrapper">
+      {count % 37 === 0 ? <div className="t1-wrapper" /> : null}
+      {count % 59 === 0 ? <div className="t2-wrapper" /> : null}
+      {count % 29 === 0 ? <div className="t3-wrapper" /> : null}
+      {count % 43 === 0 ? <div className="t4-wrapper" /> : null}
+      {count % 97 === 0 ? <div className="t5-wrapper" /> : null}
+      {count % 47 === 0 ? <div className="t6-wrapper" /> : null}
+      {count % 11 === 0 ? <div className="t7-wrapper" /> : null}
+      {count % 3 === 0 ? <div className="t8-wrapper" /> : null}
+      {count % 71 === 0 ? <div className="t9-wrapper" /> : null}
+
+      <div className="center-wrapper">
+        <div className="center-content">
+          <TextType />{" "}
+        </div>
+      </div>
     </div>
   );
 };
